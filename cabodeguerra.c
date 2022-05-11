db3141c5aeea35efcd0c8769f5732d255f03734b
Heitor Stein <37478781+hhs0001@users.noreply.github.com>
2022-05-11 11:32:41 -0300

    Update cabodeguerra.c

--- cabodeguerra.c
+++ cabodeguerra.c
@@ -3,36 +3,28 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <semaphore.h>
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
-#define NUMCONS		2
-#define NUMPROD		2
-#define BUFFERSIZE	1000
 
-//int sorteio(int MIN, int MAX){
-//	srand48(time(0));
-//	int n = (int) (drand48()*(MAX - MIN + 1)) + MIN;
-//	return n;
-//}
+int sorteio(int MIN, int MAX){
+	srand48(time(0));
+	int n = (int) (drand48()*(MAX - MIN + 1)) + MIN;
+	return n;
+}
 
-pthread_t cons[NUMCONS];
-pthread_t prod[NUMPROD];
-int buffer[BUFFERSIZE];
 int currentidx;
 pthread_mutex_t buffer_mutex;
-sem_t buffer_full, buffer_empty;
 void *consumidor(void *arg);
 void *produtor(void *arg);
 
 //declaração das variaveis globais
 int SEU_RA = 202010655;
-char primiraLetra[1] = "H";
-char segundaLetra[1] = "H";
-char terceiraLetra[1] = "S";
-
-char cabodeguerra[87][1];
+char primiraLetra = 'H';
+char segundaLetra = 'H';
+char terceiraLetra = 'S';
+int loop = 1;
+char cabodeguerra[87];
 
 int primeiraLetraPOS = 42;
 int ultimaLetraPOS = 44;
@@ -45,7 +37,7 @@ int verificador(){
 		return 1;
 	}
 	if (numerocontrole == 86){
-		return 1;
+		return 2;
 	}
 	return 0;
 }
@@ -56,16 +48,16 @@ int verificador(){
 
 void clrscr()
 {
-   system("@cls||clear");
+   system("clear");
 }
 
 //andar para a esquerda com o cabodeguerra
 
 int andarParaEsquerda() {
-	strcpy(cabodeguerra[numerocontrole], terceiraLetra);
-	strcpy(cabodeguerra[numerocontrole + 1], "_");
-	strcpy(cabodeguerra[numerocontrole - 1], segundaLetra);
-	strcpy(cabodeguerra[numerocontrole - 2], primiraLetra);
+	cabodeguerra[numerocontrole] = terceiraLetra;
+	cabodeguerra[numerocontrole - 1] = segundaLetra;
+	cabodeguerra[numerocontrole - 2] = primiraLetra;
+	cabodeguerra[numerocontrole + 1] = '_';
 	numerocontrole--;
 	return 0;
 }
@@ -73,106 +65,95 @@ int andarParaEsquerda() {
 //andar para a direita com o cabodeguerra
 
 int andarParaDireita() {
-	strcpy(cabodeguerra[numerocontrole], primiraLetra);
-	strcpy(cabodeguerra[numerocontrole + 1], segundaLetra);
-	strcpy(cabodeguerra[numerocontrole + 2], terceiraLetra);
-	strcpy(cabodeguerra[numerocontrole - 1], "_");
+	cabodeguerra[numerocontrole] = primiraLetra;
+	cabodeguerra[numerocontrole + 1] = segundaLetra;
+	cabodeguerra[numerocontrole + 2] = terceiraLetra;
+	cabodeguerra[numerocontrole - 1] = '_';
 	numerocontrole++;
 	return 0;
 }
 
 int main(int argc, char **argv) {
-	for(int contador = 0; contador <= 87; contador++)
+	int numeroConsumidores = sorteio(7, 22);
+	int numeroProdutores = sorteio(7, 22);
+	while (numeroConsumidores == numeroProdutores) {
+		numeroConsumidores = sorteio(7, 22);
+		numeroProdutores = sorteio(7, 22);
+	}
+	for(int contador = 0; contador <= 86; contador++)
   	{
 		if (contador == 42)
 		{
-			strcpy(cabodeguerra[contador], primiraLetra);
+			cabodeguerra[contador] = primiraLetra;
 		}
 		else if (contador == 43)
 		{
-			strcpy(cabodeguerra[contador], segundaLetra);
+			cabodeguerra[contador] = segundaLetra;
 		}
 		else if (contador == 44)
 		{
-			strcpy(cabodeguerra[contador], terceiraLetra);
+			cabodeguerra[contador] = terceiraLetra;
 		}
 		else{
-			strcpy(cabodeguerra[contador], "_");
+			cabodeguerra[contador] = '_';
 		}
 	}
+	cabodeguerra[87] = '\0';
 	int i; 
 	time_t t;
 	srand48(time(&t));
 	currentidx = 0;
 	pthread_mutex_init(&buffer_mutex, NULL);
-	sem_init(&buffer_full, 0, 0);
-	sem_init(&buffer_empty, 0, 0);
-	for(i = 0; i < NUMCONS; i++) {
-		pthread_create(&(cons[i]), NULL, consumidor, NULL);
+	for(i = 0; i < numeroConsumidores; i++) {
+		pthread_t cons;
+		pthread_create(&cons, NULL, consumidor, NULL);
 	}
-	for(i = 0; i < NUMPROD; i++) {
-		pthread_create(&(prod[i]), NULL, produtor, NULL);
+	for(i = 0; i < numeroProdutores; i++) {
+		pthread_t prod;
+		pthread_create(&prod, NULL, produtor, NULL);
 	}
-	for(i = 0; i < NUMCONS; i++) {
-		pthread_join(cons[i], NULL);
-	}
-	for(i = 0; i < NUMPROD; i++) {
-		pthread_join(prod[i], NULL);
+	while(loop == 1) {
+		clrscr();
+		printf("%s\n", cabodeguerra);
+		if (verificador() != 0) {
+			if (verificador() == 1) {
+				printf("%s\n", "Os consumidores ganharam o jogo!");
+				printf("O numero de consumidores foi: ");
+				printf("%d\n", numeroConsumidores);
+				printf("O numero de produtores foi: ");
+				printf("%d\n", numeroProdutores);
+			}
+			if (verificador() == 2) {
+				printf("%s\n", "Os produtores ganharam o jogo!");
+				printf("O numero de consumidores foi: ");
+				printf("%d\n", numeroConsumidores);
+				printf("O numero de produtores foi: ");
+				printf("%d\n", numeroProdutores);
+			}
+			loop = 0;
+		}
 	}
 }
 
 void *produtor(void *arg) {
-	int n;
 	while(1) {
-		n = (int) (drand48() * 1000.0);
-		pthread_mutex_lock(&buffer_mutex);
-		if(currentidx == BUFFERSIZE) {
+		if(verificador() == 0) {
+			pthread_mutex_lock(&buffer_mutex);
+			andarParaDireita();
 			pthread_mutex_unlock(&buffer_mutex);
-			sem_wait(&buffer_full);
-		} else {
-			buffer[currentidx++] = n;
-			if(currentidx == 1) sem_post(&buffer_empty);
-			pthread_mutex_unlock(&buffer_mutex);
-			//printf("Produzindo numero %d\n", n);
-			if (verificador() == 0) {
-				//printf("%s\n", *cabodeguerra);
-				printf("sou o produtor: %d\n", numerocontrole);
-				andarParaDireita();
-			}
-			else {
-				printf("Os vencedores foram os consumidores\n");
-			}
+			usleep((long) (drand48() * SEU_RA/89.0));
 		}
-		usleep((long) (drand48() * SEU_RA/89.0));
-		//clrscr();
 	}
 }
 
 
 void *consumidor(void *arg) {
-	int n;
 	while(1) {
-		pthread_mutex_lock(&buffer_mutex);
-		if(currentidx > 0) {
-			n = buffer[--currentidx];
-			if(currentidx == (BUFFERSIZE - 1))
-				sem_post(&buffer_full);
+		if(verificador() == 0) {
+			pthread_mutex_lock(&buffer_mutex);
+			andarParaEsquerda();
 			pthread_mutex_unlock(&buffer_mutex);
-			//printf("Consumindo numero %d\n", n);
-			if (verificador() == 0) {
-				//printf("%s", *cabodeguerra);
-				printf("sou o consumidor: %d\n", numerocontrole);
-				andarParaEsquerda();
-			} else
-			{
-				printf("Os vencedores foram os consumidores\n");
-			}
-			
-		} else {
-			pthread_mutex_unlock(&buffer_mutex);
-			sem_wait(&buffer_empty);
+			usleep((long) (drand48() * SEU_RA/89.0));
 		}
-		usleep((long) (drand48() * SEU_RA/89.0));
-		//clrscr();
 	}
 }
